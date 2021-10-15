3399273eb898ea58ea0e81c114784758130aab52
Gabe Fierro <gtfierro@cs.berkeley.edu>
2021-10-14 20:46:57 -0600

    add example, logging

--- mortar-parquet-client/client.py
+++ mortar-parquet-client/client.py
@@ -19,14 +19,13 @@ class Client:
         self.store.default_union = True # queries default to the union of all graphs
         self.store.open("/tmp/graph.db")
         for ttlfile in glob.glob(str(self.graph_folder / "*.ttl")):
-            if 'bldg1' not in ttlfile:
-                continue
             graph_name = os.path.splitext(os.path.basename(ttlfile))[0]
             graph_name = f"urn:{graph_name}#"
             graph = self.store.graph(graph_name)
-            print(f"Loaded {ttlfile} => {graph_name}")
+            print(f"Loading {ttlfile} => ", end='', flush=True)
             graph.parse(ttlfile, format="ttl")
             graph.parse("https://github.com/BrickSchema/Brick/releases/download/nightly/Brick.ttl", format="ttl")
+            print(f"Done as {graph_name}")
         
     def sparql(self, query, sites=None):
         if sites is None:
@@ -63,6 +62,7 @@ class Client:
         dfs = []
         for batch in self.ds.to_batches(filter=f):
             df = batch.to_pandas()
+            print(f"Downloaded batch of {len(df)} records")
             limit -= len(df)
             dfs.append(df)
             if limit <= 0:
@@ -74,4 +74,13 @@ class Client:
         return functools.reduce(lambda x, y: pd.concat([x, y], axis=0), dfs)
 
 if __name__ == '__main__':
-    c = Client("graphs", "192.168.100.3:8333")
+    c = Client("graphs", s3_endpoint="https://parquet.mortardata.org")
+    df = c.data_sparql("""SELECT ?vav ?sen ?p  WHERE {
+        ?sen_point rdf:type/rdfs:subClassOf* brick:Temperature_Sensor ;
+            brick:timeseries [ brick:hasTimeseriesId ?sen ] .
+        ?sp rdf:type/rdfs:subClassOf* brick:Temperature_Setpoint ;
+            brick:timeseries [ brick:hasTimeseriesId ?sp ] .
+        ?vav a brick:VAV .
+        ?vav brick:hasPoint ?sen, ?sp .
+        }""", sites=["bldg1", "bldg2"], start='2016-01-01', end='2016-02-01', limit=1e6)
+    print(df.head())
