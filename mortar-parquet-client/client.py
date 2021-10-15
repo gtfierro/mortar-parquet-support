82ab826b421fd086762efcf98b9ace1342d651e6
Gabe Fierro <gtfierro@cs.berkeley.edu>
2021-10-14 20:28:00 -0600

    add client init

---
+++ mortar-parquet-client/client.py
@@ -0,0 +1,77 @@
+import os
+import pandas as pd
+import functools
+import pyarrow as pa
+from pathlib import Path
+import pyarrow.dataset as ds
+import pyarrow.compute as pc
+from pyarrow import fs
+import rdflib
+import glob
+
+
+class Client:
+    def __init__(self, graph_folder, s3_endpoint=None):
+        self.s3 = fs.S3FileSystem(endpoint_override=s3_endpoint)
+        self.graph_folder = Path(graph_folder)
+        self.ds = ds.parquet_dataset('data/_metadata', partitioning='hive', filesystem=self.s3)
+        self.store = rdflib.Dataset()
+        self.store.default_union = True # queries default to the union of all graphs
+        self.store.open("/tmp/graph.db")
+        for ttlfile in glob.glob(str(self.graph_folder / "*.ttl")):
+            if 'bldg1' not in ttlfile:
+                continue
+            graph_name = os.path.splitext(os.path.basename(ttlfile))[0]
+            graph_name = f"urn:{graph_name}#"
+            graph = self.store.graph(graph_name)
+            print(f"Loaded {ttlfile} => {graph_name}")
+            graph.parse(ttlfile, format="ttl")
+            graph.parse("https://github.com/BrickSchema/Brick/releases/download/nightly/Brick.ttl", format="ttl")
+        
+    def sparql(self, query, sites=None):
+        if sites is None:
+            res = self.store.query(query)
+            rows = list(res)
+            df = pd.DataFrame.from_records(
+                rows, columns=[str(c) for c in res.vars]
+            )
+            return df
+        dfs = []
+        for site in sites:
+            graph_name = f"urn:{site}#"
+            graph = self.store.graph(graph_name)
+            res = graph.query(query)
+            rows = list(res)
+            df = pd.DataFrame.from_records(
+                rows, columns=[str(c) for c in res.vars]
+            )
+            df["site"] = site
+            dfs.append(df)
+        if len(dfs) == 0:
+            return pd.DataFrame()
+        if len(dfs) == 1:
+            return dfs[0]
+        return functools.reduce(lambda x, y: pd.concat([x, y], axis=0), dfs)
+            
+        
+    def data_sparql(self, sparql, sites=None, start=None, end=None, limit=None):
+        res = self.sparql(sparql)
+        start = pd.to_datetime("2000-01-01T00:00:00Z" if not start else start)
+        end = pd.to_datetime("2100-01-01T00:00:00Z" if not end else end)
+        uuids = list(set([str(item) for row in res.values for item in row]))
+        f = (ds.field('uuid').isin(uuids)) & (ds.field("time") <= pa.scalar(end)) & (ds.field("time") >= pa.scalar(start))
+        dfs = []
+        for batch in self.ds.to_batches(filter=f):
+            df = batch.to_pandas()
+            limit -= len(df)
+            dfs.append(df)
+            if limit <= 0:
+                break
+        if len(dfs) == 0:
+            return pd.DataFrame()
+        if len(dfs) == 1:
+            return dfs[0]
+        return functools.reduce(lambda x, y: pd.concat([x, y], axis=0), dfs)
+
+if __name__ == '__main__':
+    c = Client("graphs", "192.168.100.3:8333")
